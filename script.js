4d347f4e33e8a76bad59e44e4d46282c5c0c09f1
Sveta Demkovich <svetlana.demkovich@gmail.com>
2020-06-04 18:00:03 -0700

    update localStorage function

--- script.js
+++ script.js
@@ -8,48 +8,48 @@ $(document).ready(function() {
     
     // Add function to check localStorage for userInput and render them to the page
     function LocalStorage() {
-        var userInput8 = JSON.parse(localStorage.getItem("userInput8"));
+        var userInput8 = localStorage.getItem("userInput8");
         console.log(userInput8);
         if (userInput8 !== null) {
           $("#8").text(userInput8);
         };
-        var userInput9 = JSON.parse(localStorage.getItem("userInput9"));
+        var userInput9 = localStorage.getItem("userInput9");
         if (userInput9 !== null) {
           $("#9").text(userInput9);
         };
-        var userInput10 = JSON.parse(localStorage.getItem("userInput10"));
+        var userInput10 = localStorage.getItem("userInput10");
         if (userInput10 !== null) {
           $("#10").text(userInput10);
         };
-        var userInput11 = JSON.parse(localStorage.getItem("userInput11"));
+        var userInput11 = localStorage.getItem("userInput11");
         if (userInput11 !== null) {
           $("#11").text(userInput11);
         };
-        var userInput12 = JSON.parse(localStorage.getItem("userInput12"));
+        var userInput12 = localStorage.getItem("userInput12");
         if (userInput12 !== null) {
           $("#12").text(userInput12);
         };
-        var userInput13 = JSON.parse(localStorage.getItem("userInput13"));
+        var userInput13 = localStorage.getItem("userInput13");
         if (userInput13 !== null) {
           $("#13").text(userInput13);
         };
-        var userInput14 = JSON.parse(localStorage.getItem("userInput14"));
+        var userInput14 = localStorage.getItem("userInput14");
         if (userInput14 !== null) {
           $("#14").text(userInput14);
         };
-        var userInput15 = JSON.parse(localStorage.getItem("userInput15"));
+        var userInput15 = localStorage.getItem("userInput15");
         if (userInput15 !== null) {
           $("#15").text(userInput15);
         };
-        var userInput16 = JSON.parse(localStorage.getItem("userInput16"));
+        var userInput16 = localStorage.getItem("userInput16");
         if (userInput16 !== null) {
           $("#16").text(userInput16);
         };
-        var userInput17 = JSON.parse(localStorage.getItem("userInput17"));
+        var userInput17 = localStorage.getItem("userInput17");
         if (userInput17 !== null) {
           $("#17").text(userInput17);
         };
-        var userInput18 = JSON.parse(localStorage.getItem("userInput18"));
+        var userInput18 = localStorage.getItem("userInput18");
         if (userInput18 !== null) {
           $("#18").text(userInput18);
         };
@@ -63,8 +63,9 @@ $(document).ready(function() {
         console.log(eventID);
         var currentTableCell = event.currentTarget.parentElement.previousElementSibling;
         console.log(currentTableCell.innerText);
-        localStorage.setItem(["userInput"+ eventID], JSON.stringify(currentTableCell.innerText));
+        localStorage.setItem(["userInput"+ eventID], currentTableCell.innerText);
        
+        
     });
     
     // Setting interval to run checkTime function every 10 seconds; at the beginning of the hour, runs function to update screen
